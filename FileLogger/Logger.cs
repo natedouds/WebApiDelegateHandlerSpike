5df466c8c87685226d845615ac07306345812b98
Nathan Douds <nate.douds@gmail.com>
2017-02-19 08:05:06 -0600

    working file logger project

---
+++ FileLogger/Logger.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+using System.IO;
+
+namespace FileLogger
+{
+    public class Logger
+    {
+        //Method to create file
+        public void CreateFile(string directory, string file)
+        {
+            //Create directory (only creates if doesn't exist)
+            var di = new DirectoryInfo(directory);
+            di.Create();
+
+            //Create a file to write to
+            using (StreamWriter sw = File.AppendText(directory + file))
+            {
+                sw.WriteLine("Hello World!");
+            }
+        }
+    }
+}
--- WebApiDelegateExample/Global.asax.cs
+++ WebApiDelegateExample/Global.asax.cs
@@ -1,7 +1,11 @@
-﻿using System.Web.Http;
+﻿using System.Web;
+using System.Web.Configuration;
+using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using FileLogger;
 
 namespace WebApiDelegateExample
 {
@@ -13,6 +17,15 @@ namespace WebApiDelegateExample
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            TestLogger();
+        }
+
+        private void TestLogger()
+        {
+            var dir = HttpRuntime.BinDirectory + "logging\\";
+            var file = WebConfigurationManager.AppSettings["loggerFileName"];
+            var logger = new Logger();
+            logger.CreateFile(dir, file);
         }
     }
 }
